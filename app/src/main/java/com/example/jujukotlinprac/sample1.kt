dd1a243f7b861c81e782104061008f7568ec2dd7
JuHan <1004sunye@naver.com>
2022-12-10 03:20:22 +0900

    1210

--- app/src/main/java/com/example/jujukotlinprac/sample1.kt
+++ app/src/main/java/com/example/jujukotlinprac/sample1.kt
@@ -18,3 +18,14 @@ fun add(a : Int, b : Int) : Int {
 }
 
 //2. val vs var
+fun hi() {
+    val a : Int = 10
+
+    var b : Int = 9
+
+    //  a = 100     -> val은 변경 불가
+    b = 100         // var은 변경 가능
+
+    val c = 100     // 좌변의 Int 생략 가능
+    var d = 100     // 좌변의 Int 생략 가능
+}
